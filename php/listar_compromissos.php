df36d27b770b4d65adab8b8a649925d61cf29938
techtuxbr <victordevtb@gmail.com>
2018-01-24 13:20:03 -0200

    Primeiro commit

---
+++ php/listar_compromissos.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Content-Type: application/json');
+
+require 'conexao.php';
+
+$sql = "select * from compromissos";
+
+$query = $conn->query($sql);
+$rows = $query->fetchAll(PDO::FETCH_ASSOC);
+
+$resultado = [];
+foreach($rows as $i => $row) {
+	$resultado[$i]['id'] = (int)$row['id'];
+	$resultado[$i]['title'] = $row['nome'];
+	$resultado[$i]['start'] = date("Y-m-d\TH:i:s", strtotime($row['inicio']));
+	$resultado[$i]['end'] = date("Y-m-d\TH:i:s", strtotime($row['fim']));
+}
+
+echo json_encode($resultado);
+exit;
